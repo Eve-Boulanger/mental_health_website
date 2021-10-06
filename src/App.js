3a12b87ae7d848326da4d099969be130d37c6940
Eve-Boulanger <eboulang@uwaterloo.ca>
2021-10-06 19:18:37 -0400

    All main page development

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/Pages/Home";
+import { Activities } from "./components/Pages/Activities";
+import { Resources } from "./components/Pages/Resources";
+import { Support } from "./components/Pages/Support";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/activities" component={Activities} />
+            <Route path="/resources" component={Resources} />
+            <Route path="/support" component={Support} />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Pages/Activities.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Activities = () => {
+  return (
+    <div>
+      <h1>Activities</h1>
+    </div>
+  );
+};
