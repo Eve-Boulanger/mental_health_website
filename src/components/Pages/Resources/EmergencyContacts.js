bf6a44a33ae6285f976eb67f6678c4668100b853
Nadia <bn3nguye@uwaterloo.ca>
2023-02-28 20:41:31 -0500

    started cleaning title styling for Resources Page

--- src/components/Pages/Resources/EmergencyContacts.js
+++ src/components/Pages/Resources/EmergencyContacts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './css/EmergencyContacts.css'
+import '../../Styles/GeneralStyles.css'
 import thinking from '../../Images/thinking-man.jpg'
 import thinking2 from '../../Images/thinking-person.png'
 import thinking3 from '../../Images/thinking-person-string.png'
@@ -11,9 +12,9 @@ export const EmergencyContacts = () => {
          <div className='row'>
         <div className="column">
             <div className="contacts">
-            <h2><span className="black-title-emg">Emergency</span><span className="blue-title-emg"> Support</span></h2>
+            <h2><span className="black-title title-text">Emergency</span><span className="blue-title title-text"> Support</span></h2>
             <p className="call"><b>Call 211 to be directed to the best resource for you!</b></p>
-            <h2><span className="black-title-emg">Talk to Someone</span><span className="blue-title-emg"> in Waterloo</span></h2>
+            <h2><span className="black-title title-text">Talk to Someone</span><span className="blue-title title-text"> in Waterloo</span></h2>
             <ul className="more-contacts">
             <li>Supportive and Confidential Listening (519-745-1166)</li>
               <li>Here 24/7: Addictions, Mental Health and Crisis Services (1-844-437-3247)</li>
@@ -21,7 +22,7 @@ export const EmergencyContacts = () => {
               <li>24-hour Support Line for Sexual Violence Survivors (519-741-8633)</li>
               <li>Mental Health and Community Referral Information (519-744-5594)</li>
             </ul>
-            <h2><span className="black-title-emg">Navigate</span><span className="blue-title-emg"> To...</span></h2>
+            <h2><span className="black-title title-text">Navigate</span><span className="blue-title title-text"> To...</span></h2>
             </div>
         </div>
         <div className="column">
