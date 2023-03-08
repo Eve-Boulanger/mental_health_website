29135224c7bec12cb74874bdeceb737293dcc56a
Nadia <bn3nguye@uwaterloo.ca>
2023-03-07 20:50:50 -0500

    started organizing headers

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -8,29 +8,34 @@ import donate from'../Pages/img/donate.png';
 export const Home = () => {
   return (
     <div>
-      <div class = "containerHome light-blue-background">
-        <h1><span class = "blue-title title-text">PRIORITIZE</span><span class = "black-title title-text"> MENTAL HEALTH.</span></h1> 
-        <h1><span class = "blue-title title-text">BREAK THE</span><span class = "black-title title-text"> STIGMA.</span></h1>
-        <h1><span class = "blue-title title-text">ASK FOR</span><span class = "black-title title-text"> HELP.</span></h1> 
-        <h1><span class = "blue-title title-text">PROGRESS</span><span class = "black-title title-text"> OVER PERFECTION.</span></h1>
-      </div>
-      <div class = "headerImg">
-        <img src={brain} alt="brain exercise"/>
-      </div>
-      <div class = "aboutContainerHome">
-        <h2>About</h2>
-        <p>Welcome to YouWaterloo! This page gives students a platform to work on their mental health. Mental health is just as important as physical health. If you have the flu, you see a doctor, so if you are feeling mentally unwell you seek professional help no matter how big or small the problem. Practicing mental health is important for everyone, just as we exercise our bodies we need to exercise our minds.</p>
-        <p><span class = "bold">This website was created to promote positive mental health practices, raise awareness about the topic, and fundraise for mental health research and for students who are in need of mental health resources beyond those that are free through the university.</span> Hats, patches and pins are being provided by the Macartney Foundation. The Macartney accesories were created by a University of Waterloo student who lost his father at a young age due to a mental illness. These accessories are a symbol that you are open to talk about mental health or that you need someone to talk to. Wearing these symbols supports positive mental health in the community and starts the conversation. Break down the stigma and take control of mental health by donating, practicing the outlined activities or just reading up on the subject.</p>
-      </div>
-      <div class = "donateButtonHome">
-        <div class = "donateButtonHomeBUTTON">
-          <Link to="/support"><button>
-              DONATE NOW <img src={donate} alt="donate hand icon"/>
-            </button>
-          </Link>
+      <div class = "row light-blue-background">
+        <div class = "column">
+          <h1><span class = "blue-title title-text">PRIORITIZE</span><span class = "black-title title-text"> MENTAL HEALTH.</span></h1> 
+          <h1><span class = "blue-title title-text">BREAK THE</span><span class = "black-title title-text"> STIGMA.</span></h1>
+          <h1><span class = "blue-title title-text">ASK FOR</span><span class = "black-title title-text"> HELP.</span></h1> 
+          <h1><span class = "blue-title title-text">PROGRESS</span><span class = "black-title title-text"> OVER PERFECTION.</span></h1>
         </div>
-        <p> Donate to support mental health research and help students in need. </p>
+        <div class = "column">
+          <div class = "header-image">
+            <img src={brain} alt="brain exercise"/>
+          </div>
+        </div>
+        <div class = "aboutContainerHome">
+          <h2>About</h2>
+          <p>Welcome to YouWaterloo! This page gives students a platform to work on their mental health. Mental health is just as important as physical health. If you have the flu, you see a doctor, so if you are feeling mentally unwell you seek professional help no matter how big or small the problem. Practicing mental health is important for everyone, just as we exercise our bodies we need to exercise our minds.</p>
+          <p><span class = "bold">This website was created to promote positive mental health practices, raise awareness about the topic, and fundraise for mental health research and for students who are in need of mental health resources beyond those that are free through the university.</span> Hats, patches and pins are being provided by the Macartney Foundation. The Macartney accesories were created by a University of Waterloo student who lost his father at a young age due to a mental illness. These accessories are a symbol that you are open to talk about mental health or that you need someone to talk to. Wearing these symbols supports positive mental health in the community and starts the conversation. Break down the stigma and take control of mental health by donating, practicing the outlined activities or just reading up on the subject.</p>
+        </div>
+        <div class = "donateButtonHome">
+          <div class = "donateButtonHomeBUTTON">
+            <Link to="/support"><button>
+                DONATE NOW <img src={donate} alt="donate hand icon"/>
+              </button>
+            </Link>
+          </div>
+          <p> Donate to support mental health research and help students in need. </p>
+        </div>
       </div>
+
       <div className = "middleAndBackTextHome">
         <h2>Activities</h2>
         <p>Exercise your mind with meditation, daily reflection, and positive thinking.</p>
--- src/components/Pages/Resources/EmergencyContacts.js
+++ src/components/Pages/Resources/EmergencyContacts.js
@@ -9,39 +9,39 @@ import {Link} from "react-router-dom";
 export const EmergencyContacts = () => {
     return(
     <div className="wrapper light-blue-background">
-         <div className='row'>
-        <div className="column">
-            <div className="contacts">
-            <h2><span className="black-title title-text">Emergency</span><span className="blue-title title-text"> Support</span></h2>
-            <p className="call"><b>Call 211 to be directed to the best resource for you!</b></p>
-            <h2><span className="black-title title-text">Talk to Someone</span><span className="blue-title title-text"> in Waterloo</span></h2>
-            <ul className="more-contacts">
-            <li>Supportive and Confidential Listening (519-745-1166)</li>
-              <li>Here 24/7: Addictions, Mental Health and Crisis Services (1-844-437-3247)</li>
-              <li>Good2Talk Support Line for Post-secondary Students (1-866-925-5454)</li>
-              <li>24-hour Support Line for Sexual Violence Survivors (519-741-8633)</li>
-              <li>Mental Health and Community Referral Information (519-744-5594)</li>
-            </ul>
-            <h2><span className="black-title title-text">Navigate</span><span className="blue-title title-text"> To...</span></h2>
+        <div className='row'>
+          <div className="column">
+              <div className="contacts">
+              <h2><span className="black-title title-text">Emergency</span><span className="blue-title title-text"> Support</span></h2>
+              <p className="call"><b>Call 211 to be directed to the best resource for you!</b></p>
+              <h2><span className="black-title title-text">Talk to Someone</span><span className="blue-title title-text"> in Waterloo</span></h2>
+              <ul className="more-contacts">
+              <li>Supportive and Confidential Listening (519-745-1166)</li>
+                <li>Here 24/7: Addictions, Mental Health and Crisis Services (1-844-437-3247)</li>
+                <li>Good2Talk Support Line for Post-secondary Students (1-866-925-5454)</li>
+                <li>24-hour Support Line for Sexual Violence Survivors (519-741-8633)</li>
+                <li>Mental Health and Community Referral Information (519-744-5594)</li>
+              </ul>
+              <h2><span className="black-title title-text">Navigate</span><span className="blue-title title-text"> To...</span></h2>
+              </div>
+          </div>
+          <div className="column">
+              <div className="parent">
+                  <img className="img1-emergency" src={thinking3} alt="Thinking 3" />
+                  <img className="img1-emergency" src={thinking} alt="Thinking 1" />
+                  <img className="img1-emergency" src={thinking2} alt="Thinking 2" />
+              </div>
+          </div>
+          <div className = "navigateTo">
+            <div className = "call">
+              <a href="#suicidal-thoughts-info"><b className = "navigateToText">Helping Someone With Suicidal Thoughts</b></a>
             </div>
-        </div>
-        <div className="column">
-            <div className="parent">
-                <img className="img1-emergency" src={thinking3} alt="Thinking 3" />
-                <img className="img1-emergency" src={thinking} alt="Thinking 1" />
-                <img className="img1-emergency" src={thinking2} alt="Thinking 2" />
+            <div className = "call">
+              <a href="#panic-attack"><b className = "navigateToText">Helping Someone Having a Panic Attack</b></a>
             </div>
-          </div>
-        </div>
-        <div className = "navigateTo">
-          <div className = "call">
-            <a href="#suicidal-thoughts-info"><b className = "navigateToText">Helping Someone With Suicidal Thoughts</b></a>
-          </div>
-          <div className = "call">
-            <a href="#panic-attack"><b className = "navigateToText">Helping Someone Having a Panic Attack</b></a>
-          </div>
-          <div className = "call">
-            <a href="#anxiety-info"><b className = "navigateToText">Helping Someone With Anxiety</b></a>
+            <div className = "call">
+              <a href="#anxiety-info"><b className = "navigateToText">Helping Someone With Anxiety</b></a>
+            </div>
           </div>
         </div>
     </div>
