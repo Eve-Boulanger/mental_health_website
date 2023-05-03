53f3d1c07ffe778a35e1c18a0d027e2d0fea6bcc
Nadia <bn3nguye@uwaterloo.ca>
2023-05-02 20:56:12 -0400

    continue clean-up

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -45,35 +45,41 @@ export const Home = () => {
         </div>
       </div>
 
-      <div className = "middleAndBackTextHome">
-        <h2>Activities</h2>
-        <p>Exercise your mind with meditation, daily reflection, and positive thinking.</p>
-        <div class = "readMoreMiddle">
-          <Link to="/activities"><button>
-            Read More.
-          </button>
-          </Link>
-        </div>
-      </div>
-      <div className = "leftTextHome">
+      <div className = "row">
+
+        <div className = "column">
         <h2>Resources</h2>
         <p>There are a number of free mental health resources available in the community. This page outlines hotline numbers as well as information about mental health services.</p>
-        <div class = "readMoreLeft">
-          <Link to="/resources"><button>
-            Read More.
-          </button>
-          </Link>
+          <div class = "readMoreLeft">
+            <Link to="/resources"><button>
+              Read More.
+            </button>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className = "rightTextHome">
+
+        <div className = "column">
+        <h2>Activities</h2>
+        <p>Exercise your mind with meditation, daily reflection, and positive thinking.</p>
+          <div class = "readMoreMiddle">
+            <Link to="/activities"><button>
+              Read More.
+            </button>
+            </Link>
+          </div>
+        </div>
+
+        <div className = "column">
         <h2>Support</h2>
         <p>All proceeds are donated to mental health research as well as to provide money to students who require mental health resources beyond those that are free through the university.</p>
-        <div class = "readMoreRight">
-          <Link to="/support"><button>
-            Read More.
-          </button>
-          </Link>
+          <div class = "readMoreRight">
+            <Link to="/support"><button>
+              Read More.
+            </button>
+            </Link>
+          </div>
         </div>
+
       </div>
     </div>
   );
