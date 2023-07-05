a3be085b169b5705b264939fad0c186703b37453
Eve-Boulanger <eboulang@uwaterloo.ca>
2023-07-04 20:51:45 -0400

    Resources page less text and stripe start

--- src/components/Pages/Activities.js
+++ src/components/Pages/Activities.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../Styles/Activities.css";
 import { BsChevronCompactRight } from 'react-icons/bs'
+import plant_head from'../Images/plant_head.png';
 
 export const Activities = () => {
   return (
@@ -14,24 +15,42 @@ export const Activities = () => {
           
         
         <div className="body">
-          <div className="activities-card">
-            <h3>Guide to Meditation</h3>
-            <a href="https://www.nytimes.com/guides/well/how-to-meditate" target="_blank" rel="noreferrer">See More<BsChevronCompactRight/></a>
-            <p>This New York Times article has lots of information about what meditation is and also has many different guided audio meditations to try!</p>
+          <div className = "top-row">
+            <div className="activities-card">
+              <h3>Guide to Meditation</h3>
+              <a href="https://www.nytimes.com/guides/well/how-to-meditate" target="_blank" rel="noreferrer">See More<BsChevronCompactRight/></a>
+              <p>This New York Times article has lots of information about what meditation is and also has many different guided audio meditations to try!</p>
+            </div>
+
+            <div className = "activities-card-imgs">
+              <img src={plant_head} alt="Plant Head Right Photo"></img>
+            </div>
           </div>
 
-          <div className="activities-card">
-            <h3>Mood Journal Tutorial</h3>
-            <a href="https://www.healthline.com/health/how-to-keep-mood-journal#How-to-keep-a-mood-journal" target="_blank" rel="noreferrer">See More<BsChevronCompactRight/></a>
-            <p>A mood journal is a great way to help you process and understand your emotions. This article gives instructions on how to start one!</p>
+          <div className = "middle-row">
+            <div className = "activities-card-imgs">
+              <img src={plant_head} alt="Plant Head Right Photo"></img>
+            </div>
+
+            <div className="activities-card">
+              <h3>Mood Journal Tutorial</h3>
+              <a href="https://www.healthline.com/health/how-to-keep-mood-journal#How-to-keep-a-mood-journal" target="_blank" rel="noreferrer">See More<BsChevronCompactRight/></a>
+              <p>A mood journal is a great way to help you process and understand your emotions. This article gives instructions on how to start one!</p>
+            </div>
           </div>
 
-          <div className="activities-card">
-            <h3>31 Tips to Boost Your Mental Health</h3>
-            <a href="https://www.mhanational.org/31-tips-boost-your-mental-health" target="_blank" rel="noreferrer">See More<BsChevronCompactRight/></a>
-            <p>Not all suggested mental health practices will work for you so here's a quick list of various things you
-              can try to help boost your mood!
-            </p>
+          <div className = "bottom-row">
+            <div className="activities-card">
+              <h3>31 Tips to Boost Your Mental Health</h3>
+              <a href="https://www.mhanational.org/31-tips-boost-your-mental-health" target="_blank" rel="noreferrer">See More<BsChevronCompactRight/></a>
+              <p>Not all suggested mental health practices will work for you so here's a quick list of various things you
+                can try to help boost your mood!
+              </p>
+            </div>
+
+            <div className = "activities-card-imgs">
+              <img src={plant_head} alt="Plant Head Right Photo"></img>
+            </div>
           </div>
         </div>
       </div>
@@ -46,7 +65,7 @@ export const Activities = () => {
             long so they can be easily integrated into your daily routine!</p>
         </div>
           
-        <div className="body">
+        <div className="body2"> 
           <div className="meditation-card">
             <iframe src="https://www.youtube.com/embed/ZToicYcHIOU" title="YouTube video player" frameborder="0"></iframe>
             <p>Video 1: Daily Calm Mindfulness Meditation [approx.10 minutes]</p>
@@ -69,7 +88,7 @@ export const Activities = () => {
           <h1>Interactive Quizzes</h1>
           <p>Here are some quizzes you can take to help you learn more about mental health, its practices and its importance!</p>
         </div>
-        <div className="body">
+        <div className="body2">
           <div className="quizzes-card">
             <h3>CDC - Mental Health Quiz</h3>
             <a href="https://www.cdc.gov/mentalhealth/quiz/index.htm" target="_blank" rel="noreferrer">Take Quiz<BsChevronCompactRight/></a>
---
+++ src/components/Pages/StripeContext.js
@@ -0,0 +1,12 @@
+{/* import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('YOUR_STRIPE_API_KEY');
+
+const StripeContext = ({ children }) => (
+  <Elements stripe={stripePromise}>
+    {children}
+  </Elements>
+);
+
+export default StripeContext; */}
